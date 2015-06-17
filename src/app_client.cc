34e0421668d021c232556c3ade5ed9063b3289ee
Arnaud Lacombe <lacombar@gmail.com>
2015-06-17 14:05:23 -0800

    app_client: read input for stdin

--- src/app_client.cc
+++ src/app_client.cc
@@ -5,20 +5,29 @@
 
 using boost::asio::ip::tcp;
 
-int
-app_client::run()
+bool
+app_client::process_one_line(std::string line)
 {
-	tcp::socket s(io_service);
-	tcp::resolver resolver(io_service);
+	int v;
 
-	boost::asio::connect(s, resolver.resolve({_host, _port}));
+	std::cout << line << std::endl;
 
-	fibonacci_api::request request(0, 3);
+	try {
+		v = std::stoi(line);
+	} catch (std::exception& e) {
+		return false;
+	}
+
+	/* Ensure the requested value will fit in the request */
+	if (v < 0 || v > UINT8_MAX)
+		return false;
+
+	fibonacci_api::request request(0, v);
 
 	boost::system::error_code error;
 	size_t length;
 
-	length = s.write_some(boost::asio::buffer(&request, sizeof request), error);
+	length = _sock.write_some(boost::asio::buffer(&request, sizeof request), error);
 	if (error)
 		throw boost::system::system_error(error);
 
@@ -27,7 +36,7 @@ app_client::run()
 
 	fibonacci_api::reply reply;
 
-	length = s.read_some(boost::asio::buffer(&reply, sizeof reply), error);
+	length = _sock.read_some(boost::asio::buffer(&reply, sizeof reply), error);
 	if (error)
 		throw boost::system::system_error(error);
 
@@ -36,6 +45,28 @@ app_client::run()
 
 	if (!fibonacci_api::verify_checksum(reply))
 		throw std::runtime_error("Invalid reply checksum");
+
+	if (!fibonacci_api::reply_is_valid(reply))
+		throw std::runtime_error("Computation failed");
+
+	std::cout << reply._reply << std::endl;
+
+	return true;
+}
+
+int
+app_client::run()
+{
+	std::string line;
+
+	tcp::resolver resolver(io_service);
+
+	boost::asio::connect(_sock, resolver.resolve({_host, _port}));
+
+	while (std::getline(std::cin, line)) {
+		if (!process_one_line(line))
+			break;
+	}
 
 	return 0;
 }
--- src/app_client.h
+++ src/app_client.h
@@ -11,14 +11,18 @@ class app_client : public app
 public:
 	app_client(const char *host, const char *port) :
 	    _host(host),
-	    _port(port)
+	    _port(port),
+	    _sock(io_service)
 	{
 	}
 
 	int run();
 private:
+	bool process_one_line(std::string);
+
 	const char *_host;
 	const char *_port;
+	boost::asio::ip::tcp::socket _sock;
 };
 
 #endif // APP_CLIENT_H
