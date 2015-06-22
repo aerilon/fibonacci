adfb32b6447b18384d30cb22902282cbbead641f
Arnaud Lacombe <lacombar@gmail.com>
2015-06-21 19:26:09 -0700

    Backend: avoid special case for 0
    M(1,1) of the n'th power matrix contains fibonacci's numbers (n - 1). At the
    cost of one more matrix multiplication, we can avoid the special case for 0.

--- src/Backend.cc
+++ src/Backend.cc
@@ -5,13 +5,10 @@
 uint64_t
 Backend::compute(uint8_t n)
 {
-	if (n == 0)
-		return 0;
-
 	boost::numeric::ublas::matrix<boost::multiprecision::uint1024_t> p = _m;
 
-	for (auto i = 0; i < (n - 2); i++)
+	for (auto i = 0; i < n; i++)
 		p = boost::numeric::ublas::prod(p, _m);
 
-	return static_cast<uint64_t>(p(0, 0));
+	return static_cast<uint64_t>(p(1, 1));
 }
