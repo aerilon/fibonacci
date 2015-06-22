46045cdb0480784d166228ece0436a6420569b1e
Arnaud Lacombe <lacombar@gmail.com>
2015-06-21 19:55:51 -0700

    Backend: implement a trivial store

--- src/Backend.cc
+++ src/Backend.cc
@@ -5,10 +5,33 @@
 uint64_t
 Backend::compute(uint8_t n)
 {
+	boost::multiprecision::uint1024_t v;
+
+	{
+		std::lock_guard<std::mutex> lk(_mapLock);
+
+		auto it = _map.find(n);
+		if (it != _map.end())
+			return static_cast<uint64_t>(it->second);
+	}
+
+	v = compute_slow(n);
+
+	{
+		std::lock_guard<std::mutex> lk(_mapLock);
+		_map[n] = v;
+	}
+
+	return static_cast<uint64_t>(v);
+}
+
+boost::multiprecision::uint1024_t
+Backend::compute_slow(uint8_t n)
+{
 	boost::numeric::ublas::matrix<boost::multiprecision::uint1024_t> p = _m;
 
 	for (auto i = 0; i < n; i++)
 		p = boost::numeric::ublas::prod(p, _m);
 
-	return static_cast<uint64_t>(p(1, 1));
+	return p(1, 1);
 }
--- src/Backend.h
+++ src/Backend.h
@@ -3,6 +3,9 @@
 
 #include <cstdint>
 
+#include <map>
+#include <mutex>
+
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 
@@ -22,6 +25,10 @@ public:
 	// Inclusive implementation upper-limit
 	const uint16_t upper_limit = 93;
 private:
+	boost::multiprecision::uint1024_t compute_slow(uint8_t);
+
+	std::mutex	_mapLock;
+	std::map<unsigned int, boost::multiprecision::uint1024_t>	_map;
 	boost::numeric::ublas::matrix<boost::multiprecision::uint1024_t> _m;
 };
 
